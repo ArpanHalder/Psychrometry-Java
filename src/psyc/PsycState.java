6073c47c6a07fbe3042a6ed448807d9fdb385c96
Arpan Halder <tech.halder.arpan@gmail.com>
2016-06-22 12:32:41 +0530

    Functions added

--- src/psyc/PsycState.java
+++ src/psyc/PsycState.java
@@ -7,33 +7,42 @@ public class PsycState {
 	double Td;
 	double Tw;
 	double B;
+	// TODO:: Add Vapour pressure as state descriptor. 
 	
 	public PsycState(){
 		this.Td = 30;
 		this.Tw = 25;
-		this.B = 101;
+		this.B = 101.325;
 	}
 	
-	public PsycState(double Td, double Tw){
+	public PsycState(double Td, double Tw){//inputs in degree C
 		this.Td = Td;
 		this.Tw = Tw;
-		this.B = 101;
+		this.B = 101.325;
 	}
 	
-	public PsycState(double Td, double Tw, double B){
+	public PsycState(double Td, double Tw, double B){ // inpurs in degree C and kPa
 		this.Td = Td;
 		this.Tw = Tw;
 		this.B = B;
 	}
 	
 	public double VapPressure(){
-		// Gives Vapour Pressure (kPa) Tdb,Twb (*C)Barpmetric_Pressure (kPa)
+		// Gives Partial Vapour Pressure (kPa) Tdb,Twb (*C)Barpmetric_Pressure (kPa)
 		double A = 0.000644;
-		return( SatVapPressure() - A*this.B*(Td - Tw));
+		return( this.SatVapPressure() - A*this.B*(Td - Tw));
 
 	}
-	public double SatVapPressure(){
+	public double SatVapPressure(){ // Saturated Vapour pressure
 		return(0.6105*Math.exp(17.27 * Tw / (237.3+ Tw)));
+	}
+	
+	public double MoistCont(){
+		return (0.622*this.VapPressure())/(B - this.VapPressure());
+	}
+	
+	public double SpecificVol(){
+		return (0.287 * (273.15 + Td))/ (B - this.VapPressure());
 	}
 
 }
