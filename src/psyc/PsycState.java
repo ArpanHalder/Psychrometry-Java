9c77eef26ff7053cb717c2c161c3f8d12bd9639c
Arpan Halder <tech.halder.arpan@gmail.com>
2016-06-22 18:21:09 +0530

    Class Complete and test sucessfull

--- src/psyc/PsycState.java
+++ src/psyc/PsycState.java
@@ -7,12 +7,13 @@ public class PsycState {
 	double Td;
 	double Tw;
 	double B;
-	// TODO:: Add Vapour pressure as state descriptor. 
+	// TODO:: Add Vapor pressure as state descriptor. 
 	
 	public PsycState(){
-		this.Td = 30;
-		this.Tw = 25;
-		this.B = 101.325;
+		// Empty constructor
+		this.Td = 0;
+		this.Tw = 0;
+		this.B = 0;
 	}
 	
 	public PsycState(double Td, double Tw){//inputs in degree C
@@ -21,28 +22,76 @@ public class PsycState {
 		this.B = 101.325;
 	}
 	
-	public PsycState(double Td, double Tw, double B){ // inpurs in degree C and kPa
+	public PsycState(double Td, double Tw, double B){ // inputs in degree C and kPa
 		this.Td = Td;
 		this.Tw = Tw;
 		this.B = B;
 	}
 	
-	public double VapPressure(){
-		// Gives Partial Vapour Pressure (kPa) Tdb,Twb (*C)Barpmetric_Pressure (kPa)
-		double A = 0.000644;
-		return( this.SatVapPressure() - A*this.B*(Td - Tw));
-
-	}
-	public double SatVapPressure(){ // Saturated Vapour pressure
-		return(0.6105*Math.exp(17.27 * Tw / (237.3+ Tw)));
+	//other constructors for empty class
+	
+	public void Psyc_B_Td_RH(double B, double Td, double RH){ // Unit kPa , deg C, in % 
+		this.Td = Td;
+		this.B = B;
+		double twb = Td, twb2, e = 1;
+		double Pw = this.SatVapPressure(Td) * RH/100;
+		while(e>0.001){ //Numerical solver for Wet Bulb Temp. in deg C
+			twb2 = ((Pw - (0.6105*Math.exp(17.27 * twb / (237.3+ twb)))) / (0.000644*B)) + Td ;
+			
+			e = Math.abs(twb2-twb);
+			twb = twb2;
+		}
 	}
 	
-	public double MoistCont(){
+	public double VapPressure(){
+		// Gives Partial Vapor Pressure (kPa) Tdb, Twb (*C)Barpmetric_Pressure (kPa)
+		return( this.SatVapPressure() - 0.000644*this.B*(Td - Tw));
+
+	}
+	public double SatVapPressure(){ // Saturated Vapor pressure
+		return(0.6105*Math.exp(17.27 * Tw / (237.3+ Tw)));
+	}
+	//Overloading
+	public double SatVapPressure(double T){ // Saturated Vapor pressure for temperature in call by value
+		return(0.6105*Math.exp(17.27 * T / (237.3+ T)));
+	}
+	
+	public double MoistCont(){ // kg of vapor per kg of da
 		return (0.622*this.VapPressure())/(B - this.VapPressure());
 	}
 	
 	public double SpecificVol(){
 		return (0.287 * (273.15 + Td))/ (B - this.VapPressure());
+	}
+	
+	public double Enthalpy(){ // Enthalpy in kJ / kg of da
+		return(1.005*Td + this.MoistCont()*(1.883*Td+2500));
+	}
+	
+	public double SigmaHeat(){
+		return(this.Enthalpy() - 4.18*this.MoistCont()*Tw);
+	}
+	
+	public double HumidityRatio(){
+		return(this.VapPressure()/this.SatVapPressure(Td));
+	}
+	
+	public double DuePoint(){
+		return((Math.log(this.VapPressure()/0.6105)*237.3)/(17.27-Math.log(this.VapPressure()/0.6105)));
+	}
+	
+	// Op methods
+	
+	public void Print(){
+		System.out.printf("Dry Bulb = %f, Wet Bulb = %f \nBarometric Pressure = %f\n",Td,Tw,B);
+		System.out.printf("\nVapor Pressure = %f", this.VapPressure());
+		System.out.printf("\nSaturated Vapor Pressure = %f", this.SatVapPressure());
+		System.out.printf("\nHumity = %f percent", this.HumidityRatio()*100);
+		System.out.printf("\nSpecific Volume = %f", this.SpecificVol());
+		System.out.printf("\nEnthalpy = %f", this.Enthalpy());
+		System.out.printf("\nSigma Heat = %f", this.SigmaHeat());
+		System.out.printf("\nMoist Content = %f", this.MoistCont());
+		System.out.printf("\nDue Point = %f", this.DuePoint());
 	}
 
 }
---
+++ src/test.java
@@ -0,0 +1,13 @@
+import psyc.PsycState;
+public class test {
+
+	public static void main(String[] args) {
+		// TODO: Auto-generated method stub
+		
+		PsycState Psmtry = new PsycState();
+		Psmtry.Psyc_B_Td_RH(101.325, 30, 66.964);
+		Psmtry.Print();
+
+	}
+
+}
